3905ccf5772175ca3c2dcca21890a12e5839f0df
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-03-22 12:11:31 +0900

    ADD: 대분류 전체, 하위 카테고리, 형제 카테고리 조회 api

--- app/models/CategoryModel.scala
+++ app/models/CategoryModel.scala
@@ -1,23 +1,12 @@
 package models
 
-import scala.concurrent.{ExecutionContext, Future}
-import slick.lifted.AbstractTable
-import scala.concurrent.ExecutionContext
-import dto._
 import javax.inject._
-import scala.concurrent.{ExecutionContext, Future}
-import slick.jdbc.MySQLProfile.api._
-import models.Tables._
-import scala.collection.mutable.Map
-import scala.util.{Failure, Success, Try}
-import java.security.MessageDigest
-import java.math.BigInteger
-import javax.crypto.Cipher
-import javax.crypto.spec.SecretKeySpec
-import common.encryption._
-import play.api.libs.json.Json
+import scala.concurrent.ExecutionContext
 import scala.language.postfixOps
-import slick.lifted.AbstractTable
+import slick.jdbc.MySQLProfile.api._
+
+// 카테고리에 대한 정보를 받아오는 클래스
+// categories 테이블이 아닌 v_categories 뷰에 접근한다.
 
 // TODO
 //  - db insertion 구현
@@ -25,9 +14,18 @@ import slick.lifted.AbstractTable
 class CategoryModel(db: Database)(implicit ec: ExecutionContext) {
 	
 	def getMainCategories =
-		db run sql"SELECT * FROM v_categories WHERE depth=0".as[(String, String, Int, Int)]
+		db run sql"SELECT * FROM v_categories WHERE depth=0".as[(String, String)] map (_.toList)
 	
-	def getChildrens(code: String) = ???
+	def getChildrens(code: String) = {
+		println(code)
+		val query = s"SELECT a.* FROM v_categories a INNER JOIN (SELECT * FROM v_categories WHERE code='${code}') b" +
+				" WHERE a.code LIKE concat(b.code, '%') AND a.depth = b.depth+1"
+		db run sql"#$query".as[(String, String)] map (_.toList)
+	}
 	
-	def getSiblings(code: String) = ???
+	def getSiblings(code: String) = {
+		val query = s"SELECT a.* FROM v_categories a INNER JOIN (SELECT * FROM v_categories WHERE code='${code}') b" +
+				" WHERE substr(a.code,1,1) = substr(b.code,1,1) AND a.depth = b.depth"
+		db run sql"#$query".as[(String, String)] map (_.toList)
+	}
 }
