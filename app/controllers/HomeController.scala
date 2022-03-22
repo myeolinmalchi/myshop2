cba7cc563d9215315a87a6ceb8bf42a6796fbad2
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-03-22 12:16:13 +0900

    ADD: 상품 검색을 위한 uri mapping

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -24,10 +24,12 @@ class HomeController @Inject() (protected val dbConfigProvider: DatabaseConfigPr
     private val productService = new ProductService(db)
     
     implicit val itemReads = Json.reads[ProductOptionItemDto]
+    implicit val imageRead = Json.reads[ProductImageDto]
     implicit val optionReads = Json.reads[ProductOptionDto]
     implicit val productReads = Json.reads[ProductDto]
     
     implicit val itemWrites = Json.writes[ProductOptionItemDto]
+    implicit val imageWrite = Json.writes[ProductImageDto]
     implicit val optionWrites = Json.writes[ProductOptionDto]
     implicit val productWrites = Json.writes[ProductDto]
     
