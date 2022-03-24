e109d1f760405e15e439a9390a0fb18b0a56e87a
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-03-24 15:48:47 +0900

    ADD: insertProductStock 메서드 추가(product_stock_ 테이블) MODIFY: get 메서드 고차함수로 변경, 콜백함수 분리

--- app/models/ProductModel.scala
+++ app/models/ProductModel.scala
@@ -1,21 +1,13 @@
 package models
 
-import scala.concurrent.ExecutionContext
+import cats.implicits._
 import dto._
 import javax.inject._
-import scala.concurrent.{ExecutionContext, Future}
-import slick.jdbc.MySQLProfile.api._
 import models.Tables._
-import scala.collection.mutable.Map
-import scala.util.{Failure, Success, Try}
-import java.security.MessageDigest
-import java.math.BigInteger
-import javax.crypto.Cipher
-import javax.crypto.spec.SecretKeySpec
-import common.encryption._
-import play.api.libs.json.Json
+import scala.concurrent.{ExecutionContext, Future}
 import scala.language.postfixOps
-import slick.lifted.AbstractTable
+import scala.util.{Failure, Success, Try}
+import slick.jdbc.MySQLProfile.api._
 
 @Singleton
 class ProductModel(db: Database)(implicit ec: ExecutionContext) {
@@ -25,80 +17,81 @@ class ProductModel(db: Database)(implicit ec: ExecutionContext) {
 	
 	val commonApi = new CommonModelApi(db)
 	import commonApi._
-
-	def getProductList(f: Products => Rep[Boolean]): Future[List[ProductDto]] ={
-		def getProducts =
-			select[Products, ProductDto](ProductDto.newInstance) { product => f(product) }
-
-		def getOptions(implicit product: ProductDto) =
-			select[ProductOptions, ProductOptionDto](ProductOptionDto.newInstance) { option =>
-				option.productId === product.productId }
-
-		def getItems(implicit option: ProductOptionDto) =
-			select[ProductOptionItems, ProductOptionItemDto](ProductOptionItemDto.newInstance) { item =>
-				item.productOptionId === option.productOptionId }
+	
+	def getProducts(g: ProductDto => Future[ProductDto])
+				   (implicit f: Products => Rep[Boolean]): Future[List[ProductDto]] =
+		select[Products, ProductDto](ProductDto.newInstance) { product =>
+			f(product) } flatMap(_ traverse(product => g(product)))
+	
+	def getOptions(g: ProductOptionDto => Future[ProductOptionDto])
+				  (implicit product: ProductDto): Future[ProductDto] =
+		select[ProductOptions, ProductOptionDto](ProductOptionDto.newInstance) { option =>
+			option.productId === product.productId } flatMap(_ traverse(option => g(option))) map(option =>
+				product.setOptions(option))
+	
+	def getItems(implicit option: ProductOptionDto): Future[ProductOptionDto] =
+		select[ProductOptionItems, ProductOptionItemDto](ProductOptionItemDto.newInstance) { item =>
+			item.productOptionId === option.productOptionId } map (item => option.setItems(item))
 		
-		getProducts map(_ map { implicit product => for {
-			optionList <- getOptions map (_ map { implicit option =>
-				for { itemList <- getItems } yield option setItems itemList })
-			options <- Future sequence optionList
-		} yield product setOptions options }) map (Future sequence _) flatten
+	def getProductsWithAll(implicit f: Products => Rep[Boolean]): Future[List[ProductDto]] =
+		getProducts { implicit product => getOptions { implicit option => getItems } }
+	
+	def insertProduct(p: ProductDto)(f: ProductDto => Future[ProductDto]): Future[ProductDto] = p.optionList match {
+		case Nil =>
+			Future.failed(new NoSuchElementException(s"[${p.name}] 메인옵션은 필수항목입니다."))
+		case _ => {
+			val query = Products returning Products.map(_.productId)
+			val row = ProductsRow(0, p.name, p.sellerId, p.price, p.categoryCode, p.detailInfo, p.thumbnail, 0, 0)
+			db run (query+=row) map(id => p.setProductId(id)) flatMap(f(_))
+		}
 	}
 	
-	def addProduct(implicit sellerId: String, p: ProductDto): Future[_] = {
-		def insertProduct(implicit p: ProductDto): Future[Int] = p.optionList match {
-			case Nil =>
-				Future.failed(new NoSuchElementException(s"[${p.name}] 메인옵션은 필수항목입니다."))
-			case _ => {
-				val query = (Products returning Products.map(_.productId))
-				val row = ProductsRow(0, p.name, p.sellerId, p.price, p.categoryCode, p.detailInfo, p.thumbnail, 0, 0)
-				db run (query += row).asTry map {
-					case Failure(e) => throw e
-					case Success(rs) => rs
-				}
-			}
+	def insertImage(i: ProductImageDto): Future[ProductImageDto] = {
+		val row = ProductImagesRow(i.productId, -1, i.image, i.sequence)
+		db run (ProductImages+=row)
+		Future(i)
+	}
+	
+	def insertOption(o: ProductOptionDto): Future[ProductOptionDto] = o.itemList match {
+		 case Nil => Future.failed(new Exception("error"))
+		 case _ =>
+			 val query = (ProductOptions returning ProductOptions.map(_.productOptionId))
+			 val row = ProductOptionsRow(o.productId, 0, o.name, o.optionSequence)
+			 for {
+				 optionId <- db.run(query += row)
+			 } yield o.setOptionId(optionId)
+	}
+	
+	def insertItem(i: ProductOptionItemDto): Future[ProductOptionItemDto] = {
+		val row = ProductOptionItemsRow(i.productOptionId, -1, i.name, i.itemSequence, i.surcharge, i.stock)
+		db.run(ProductOptionItems += row)
+		Future(i)
+	}
+	
+	def insertProductWithAll(p: ProductDto): Future[ProductDto] =
+		insertProduct(p) { p =>
+			p.optionList traverse { o =>
+				insertOption(o.setProductId(p.productId))
+			} flatMap ( _ traverse { o =>
+				o.itemList traverse { i =>
+					insertItem(i.setOptionId(o.productOptionId))
+				} map o.setItems
+			}) map p.setOptions
+		} flatMap { p =>
+			p.imageList traverse { i =>
+				insertImage(i.setProductId(p.productId))
+			} map p.setImages
 		}
-		
-		def insertImage(i: ProductImageDto, productId: Future[Int]): Future[Int] = productId transform {
-			case Success(id: Int) =>
-				val row = ProductImagesRow(id, -1, i.image, i.sequence)
-				Try(db.run(ProductImages += row))
-			case Failure(e) => Try(Future.failed(e))
-		} flatMap(identity)
-		
-		def insertOption(o: ProductOptionDto, productId: Future[Int]): Future[Int] = productId transform {
-			case Success(id: Int) =>
-				o.itemList match {
-					case Nil => Failure(new Exception("error"))
-					case _ =>
-						val query = (ProductOptions returning ProductOptions.map(_.productOptionId))
-						val row = ProductOptionsRow(id, 0, o.name, o.optionSequence)
-						Try(db.run(query += row))
-				}
-			case Failure(e) => Try(Future.failed(e))
-		} flatMap (identity)
-		
-		def insertItem(i: ProductOptionItemDto, optionId: Future[Int]): Future[Int] = optionId transform {
-			case Success(id: Int) =>
-				val row = ProductOptionItemsRow(id, -1, i.name, i.itemSequence, i.surcharge, i.stock)
-				Try(db.run(ProductOptionItems += row))
-			case Failure(e) => Try(Future.failed(e))
-		} flatMap(identity)
-		
-		Future(p) map { product =>
-			val productId = insertProduct
-			product.optionList foreach { option =>
-				val optionId = insertOption(option, productId)
-				option.itemList foreach { item => insertItem(item, optionId) }
-			}
-			product.imageList foreach { image => insertImage(image, productId)}
-			product
+	// BFS로 재고 정보 저장
+	def insertProductStock(p: ProductDto): Future[_] = {
+		def go(os: List[ProductOptionDto], pid: Int, depth: Int): Future[_] = os match {
+			case h :: t => h.itemList traverse { item =>
+				println(item.name)
+				val query = (ProductStock returning ProductStock.map(_.productStockId))
+				val row = ProductStockRow(0, pid, item.name, depth)
+				db run (query += row) map (go(t, _, depth + 1))}
+			case Nil => Future(pid)
 		}
-		
-//		val productId = insertProduct
-//		Future.sequence(p.optionList flatMap { option =>
-//			val optionId = insertOption(option, productId)
-//			option.itemList map { item => insertItem(item, optionId) }
-//		})
+		go(p.optionList, 0, 0)
 	}
 }
