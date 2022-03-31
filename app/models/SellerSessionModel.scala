1d31289e5ee9a5f0716fa66dd24b5a8b66a53302
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-03-31 13:37:52 +0900

    ADD: 판매자 인증 토큰을 저장하는 SellerSessionModel

---
+++ app/models/SellerSessionModel.scala
@@ -0,0 +1,26 @@
+package models
+
+import java.time.LocalDateTime
+import java.util.UUID
+import play.api.mvc.Session
+import scala.collection.mutable
+
+case class SellerSessionModel(token: String, sellerId: String, expiration: LocalDateTime)
+
+object SellerSessionModel {
+	
+	private val sessions = mutable.WeakHashMap.empty[Session, SellerSessionModel]
+	
+	def getSession(token: String): Option[SellerSessionModel] = {
+		println(sessions.size)
+		sessions.find(_._2.token == token).map(_._2)
+	}
+	
+	def remSession(session: Session): Unit = sessions-=session
+	
+	def generateToken(sellerId: String, session: Session): String = {
+		val token = s"$sellerId-token-${UUID.randomUUID().toString}"
+		sessions.put(session, SellerSessionModel(token, sellerId, LocalDateTime.now().plusHours(9)))
+		token
+	}
+}
