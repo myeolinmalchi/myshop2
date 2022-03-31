7359fe6c62d4165a0a86a9a1145b3569422e79be
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-03-31 13:42:25 +0900

    ADD: 장바구니 추가, 제거 및 수량 조절 메서드 추가 MODIFY: UserService에서 데이터베이스 접근 로직 분리

--- app/services/UserService.scala
+++ app/services/UserService.scala
@@ -12,6 +12,8 @@ import java.math.BigInteger
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
 import common.encryption._
+import models.{CartModel, UserModel}
+import play.api.libs.oauth.{ConsumerKey, OAuth, ServiceInfo}
 import scala.language.postfixOps
 import slick.lifted.AbstractTable
 
@@ -24,63 +26,56 @@ import slick.lifted.AbstractTable
 @Singleton
 class UserService(db: Database)(implicit ec: ExecutionContext) {
 	
-	def login(userId: String, userPw: String): Future[Option[Boolean]] = {
-		import SHA256._
-		db.run(Users.filter(u => u.userId === userId).result).map(_.headOption match {
+	private val userModel = new UserModel(db)
+	private val cartModel = new CartModel(db)
+	
+	def login(userId: String, userPw: String): Future[Option[Boolean]] =
+		userModel.getUserById(userId) map {
 			case Some(u) =>
-				if(u.userPw.equals(encrypt(userPw))) Some(true) // 로그인 성공
+				if (u.userPw.equals(SHA256.encrypt(userPw))) Some(true) // 로그인 성공
 				else Some(false) // 비밀번호 불일치
 			case None => None // 존재하지 않는 계정
-		})
-	}
+		}
 	
-	private def validate(user: UserDto): Future[UserDto] =  {
+	def accountValidation(user: UserDto): Future[UserDto] = {
+		def patterns(implicit key: String) = Map (
+			"userId" -> "^[a-z]+[a-z0-9]{5,19}$",
+			"userPw" -> "^(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$",
+			"name" -> "^[ㄱ-힣]+$",
+			"email" -> "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$",
+			"phone" -> "^\\d{3}-\\d{3,4}-\\d{4}$"
+		)(key)
+		def noneMatchedMsg(implicit key: String): String = Map (
+			"userId" -> "유효하지 않은 아이디입니다.",
+			"userPw" -> "유효하지 않은 비밀번호입니다.",
+			"name"  -> "유효하지 않은 이름입니다.",
+			"email" -> "유효하지 않은 이메일입니다.",
+			"phone" -> "유효하지 않은 전화번호입니다."
+		)(key)
+		def checkPattern(str: String)(implicit key: String): Future[String] =
+			if (!str.matches(patterns))
+				Future.failed(new IllegalArgumentException(noneMatchedMsg))
+			else Future.successful(str)
 		
-		def checkPattern(str: String, pattern: String, errMsg: String): Future[String] =
-			if(!str.matches(pattern)){
-				Future.failed(new IllegalArgumentException(errMsg))
-			} else Future.successful(str)
-		
-		def validUserId(userId: String): Future[String] = {
-			val pattern = "^[a-z]+[a-z0-9]{5,19}$"
-			if(!userId.matches(pattern)) {
-				Future.failed(new IllegalArgumentException("유효하지 않은 아이디입니다."))
-			} else{
-				val matches = db.run(Users.filter(u => u.userId === userId).result)
-				matches.flatMap { userRows =>
-					if (userRows.isEmpty) Future.successful(userId)
-					else throw new IllegalArgumentException("이미 존재하는 계정입니다.")
-				}
-			}
-		}
-		
-		def validUserPw(userPw: String): Future[String] =
-			checkPattern(userPw,
-				"^(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$",
-				"유효하지 않은 비밀번호입니다.")
-		
-		def validName(name: String): Future[String] =
-			checkPattern(name,
-				"^[ㄱ-힣]+$",
-				"유효하지 않은 이름입니다.")
-				
-		def validEmail(email: String): Future[String] = {
-			val pattern = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$"
-			if(!email.matches(pattern)) {
-				Future.failed(new IllegalArgumentException("유효하지 않은 이메일입니다."))
-			} else{
-				val matches = db.run(Users.filter(u => u.email === email).result)
-				matches.flatMap { userRows =>
-					if (userRows.isEmpty) Future.successful(email)
-					else throw new IllegalArgumentException("이미 존재하는 이메일입니다.")
-				}
-			}
-		}
-		
-		def validPhone(phone: String): Future[String] =
-			checkPattern(phone,
-				"^\\d{3}-\\d{3,4}-\\d{4}$",
-				"유효하지 않은 전화번호입니다.")
+		def validUserId(userId: String): Future[String] =
+			checkPattern(userId)("userId") transform  {
+				case Success(userId) => Try(userId)
+				case Failure(e)	 => Failure(e)
+			} flatMap { userModel getUserById(_) flatMap ({
+				case None => Future.successful(userId)
+				case Some(_) => throw new IllegalArgumentException("이미 존재하는 계정입니다.")
+			})}
+		def validUserPw(userPw: String): Future[String] = checkPattern(userPw)("userPw")
+		def validName(name: String): Future[String] = checkPattern(name)("name")
+		def validEmail(email: String): Future[String] =
+			checkPattern(email)("email") transform  {
+				case Success(email) => Try(email)
+				case Failure(e)	 => Failure(e)
+			} flatMap { userModel getUserByEmail(_) flatMap ({
+				case None => Future successful(email)
+				case Some(_) => throw new IllegalArgumentException("이미 사용중인 이메일입니다.")
+			})}
+		def validPhone(phone: String): Future[String] = checkPattern(phone)("phone")
 		
 		for {
 			userId <- validUserId(user.userId)
@@ -92,34 +87,35 @@ class UserService(db: Database)(implicit ec: ExecutionContext) {
 	}
 	
 	def register(user: UserDto): Future[Option[String]] = {
-		import SHA256._
-		validate(user).transform {
+		accountValidation(user) transform {
 			case Success(result) =>
-				db.run(Users.map(u => (u.userId, u.userPw, u.name, u.email, u.phonenumber))
-						+= (user.userId, encrypt(user.userPw), user.name, user.email, user.phonenumber))
+				userModel insertUser user
 				Try(None)
 			case Failure(e) => Try(Some(e.getMessage))
 		}
 	}
 	
 	def findId(email: String): Future[Option[String]] =
-		db.run(Users.filter(u => u.email === email).result).map(_.headOption match {
-			case Some(user) => Some(user.userId) // 아이디 찾기 성공
-			case None => None // 존재하지 않는 이메일
-		})
+		userModel getUserByEmail(email) map (_ map(_.userId))
 	
-	
-	
-	
-	def getCarts(implicit userId: String): Future[Seq[CartDto]] = {
-		def getCartsByUserId(implicit useId: String): Future[List[CartDto]] =
-			db run Carts.filter(cart => cart.userId === userId).result map(_ map { row =>
-				CartDto(row)
-			} toList )
-			
+	def getUser(userId: String): Future[UserDto] =
+		userModel getUserById(userId) map(_.getOrElse(throw new Exception()))
 		
-		db.run(Carts.filter(cart => cart.userId === userId).result).map(_.map(CartDto(_)))
-	}
+	def addCart(cart: CartDto): Future[Int] =
+		cartModel addCart(cart)
+		
+	def addQuantity(implicit cartId: Int): Future[Int] = cartModel addQuantity
+		
+	def subQuantity(implicit cartId: Int): Future[Int] = cartModel subQuantity
+	
+	def updateQuantity(q: Int)(implicit cartId: Int): Future[Int] =
+		cartModel updateQuantity(q)
+	
+	def getCarts(implicit userId: String): Future[List[CartDto]] =
+		cartModel getCartsByUserId
+		
+	def getCart(implicit cartId: Int): Future[CartDto] =
+		cartModel getCartByCartId
 	
 	def getAddress(implicit userId: String): Future[Seq[AddressDto]] =
 		db.run(UserAddresses.filter(addr => addr.userId === userId).result).map(_.map(AddressDto(_)))
