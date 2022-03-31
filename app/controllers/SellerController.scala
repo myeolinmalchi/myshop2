2af9c7ea54e1bd593d1614a3a55e46dbd3384404
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-03-31 13:36:02 +0900

    ADD: 상품 재고 조회 및 업데이트 메서드 추가 MODIFY: 로그인 인증 로직 변경(단순 id 확인에서 토큰 방식으로), 모든 메서드에 로그인 확인 로직 추가

--- app/controllers/SellerController.scala
+++ app/controllers/SellerController.scala
@@ -1,21 +1,18 @@
 package controllers
 
+import cats.data.OptionT
 import dto._
+import java.time.LocalDateTime
 import javax.inject._
+import models.{ProductModel, SellerSessionModel}
 import play.api.db.slick._
-import play.api.mvc._
 import play.api.libs.json._
-import play.libs.F.Tuple
-import scala.concurrent.{ExecutionContext, Future, blocking}
+import play.api.mvc._
+import scala.concurrent.{ExecutionContext, Future}
+import scala.language.postfixOps
 import scala.util.{Failure, Success, Try}
 import services.{ProductService, SellerService}
 import slick.jdbc.JdbcProfile
-import slick.jdbc.MySQLProfile.api._
-import slick.jdbc.MySQLProfile
-import java.security.MessageDigest
-import java.math.BigInteger
-import java.nio.file.Paths
-import scala.language.postfixOps
 
 @Singleton
 class SellerController @Inject() (protected val dbConfigProvider: DatabaseConfigProvider,
@@ -29,63 +26,98 @@ class SellerController @Inject() (protected val dbConfigProvider: DatabaseConfig
 	implicit val imageRead = Json.reads[ProductImageDto]
 	implicit val optionReads = Json.reads[ProductOptionDto]
 	implicit val productReads = Json.reads[ProductDto]
+	implicit val stockReads = Json.reads[StockDto]
 	
 	implicit val itemWrites = Json.writes[ProductOptionItemDto]
 	implicit val imageWrite = Json.writes[ProductImageDto]
 	implicit val optionWrites = Json.writes[ProductOptionDto]
 	implicit val productWrites = Json.writes[ProductDto]
-	
-	private def withSessionId(f: String => Future[Result])(implicit request: Request[AnyContent]): Future[Result] =
-		request.session.get("sellerId").map(sellerId => f(sellerId)).getOrElse(Future.successful(Ok(Json.toJson(Seq.empty[String]))))
+	implicit val stockWrites = Json.writes[StockDto]
+	implicit val stockTestWrites = Json.writes[StockResponseDto]
 	
 	private def withJsonBody[A](f: A => Future[Result])(implicit request: Request[AnyContent], reads: Reads[A]): Future[Result] = {
 		request.body.asJson.map { body =>
 			Json.fromJson[A](body) match {
 				case JsSuccess(a, path) => f(a)
 				case e @JsError(_) => {
+					println(e)
 					Future.successful(Redirect(routes.SellerController.index))
 				}
 			}
 		}.getOrElse((Future.successful(Redirect(routes.SellerController.index))))
 	}
 	
-	def index = Action { implicit request =>
-		request.session.get("sellerId") match {
-			case Some(sellerId) => Ok(views.html.seller.index(sellerId))
-			case None => Ok(views.html.seller.index(null))
-		}
+	private def extractSeller(req: RequestHeader): Future[Option[SellerDto]] = {
+		val sessionTokenOpt = req.session.get("sessionToken")
+		def swap[M](x: Option[Future[M]]): Future[Option[M]] =
+			Future.sequence(Option.option2Iterable(x)).map(_.headOption)
+		swap (sessionTokenOpt
+				.flatMap(token => SellerSessionModel.getSession(token))
+				.filter(_.expiration.isAfter(LocalDateTime.now()))
+				.map(_.sellerId)
+				.map(service.getSeller))
 	}
 	
-	def loginPage = Action { implicit request =>
-		Ok(views.html.seller.login())
+	private def withSeller[T](block: SellerDto => Future[Result])
+							 (implicit request: Request[AnyContent]): Future[Result] =
+		extractSeller(request) flatMap {
+			case Some(seller) => block(seller)
+			case None => Future(Unauthorized(views.html.seller.no_auth()))
+		}
+	
+	private def withoutSeller[T](block: => Future[Result])
+							 (implicit request: Request[AnyContent]): Future[Result] =
+		extractSeller(request) flatMap {
+			case Some(_) => Future(Unauthorized(views.html.seller.index()))
+			case None => block
+		}
+	
+	def index = Action { implicit request =>
+		Ok(views.html.seller.index())
+	}
+	
+	def loginPage = Action.async { implicit request =>
+		withoutSeller[SellerDto] {
+			Future(Ok(views.html.seller.login()))
+		}
 	}
 	
 	def login = Action.async { implicit request =>
 		withJsonBody[SellerDto] { seller =>
 			service.login(seller.sellerId, seller.sellerPw).map {
 				case Some(able) =>
-					if(able) Ok(Json.toJson(true)).withSession("sellerId" -> seller.sellerId)
+					if(able) {
+						val token = SellerSessionModel.generateToken(seller.sellerId, request.session)
+						Ok(Json.toJson(true)).withSession("sessionToken" -> token)
+					}
 					else Ok(Json.toJson(Map("error"->"비밀번호가 일치하지 않습니다.")))
 				case None => Ok(Json.toJson(Map("error" -> "존재하지 않는 계정입니다.")))
 			}
 		}
 	}
 	
-	def registerPage = Action { implicit request =>
-		Ok(views.html.seller.register())
+	def registerPage = Action.async { implicit request =>
+		withoutSeller[SellerDto]{
+			Future(Ok(views.html.seller.register()))
+		}
 	}
 	
 	def register = Action async { implicit request =>
-		withJsonBody[SellerDto] { seller =>
-			service.register(seller).map {
-				case Some(e) => Ok(Json.toJson(Map("error" -> e)))
-				case None => Ok(Json.toJson(true))
+		withoutSeller[SellerDto] {
+			withJsonBody[SellerDto] { seller =>
+				service.register(seller).map {
+					case Some(e) => Ok(Json.toJson(Map("error" -> e)))
+					case None => Ok(Json.toJson(true))
+				}
 			}
 		}
 	}
 	
-	def logout = Action { implicit request =>
-		Redirect(routes.SellerController.index).withSession(request.session - "sellerId")
+	def logout = Action.async { implicit request =>
+		withSeller[SellerDto]  { seller =>
+			SellerSessionModel.remSession(request.session)
+			Future(Redirect(routes.SellerController.index).withSession(request.session - "sessionToken"))
+		}
 	}
 	
 	def findSellerPage = Action { Ok(views.html.findUser()) }
@@ -100,17 +132,17 @@ class SellerController @Inject() (protected val dbConfigProvider: DatabaseConfig
 	}
 	
 	def productRegisterPage = Action.async { implicit request =>
-		withSessionId { sellerId =>
-			productService.getMainCategories map { categories =>
-				Future(Ok(views.html.seller.product.register(categories, sellerId)))
-			} flatten
+		withSeller[SellerDto] { seller =>
+			productService.getMainCategories flatMap { categories =>
+				Future(Ok(views.html.seller.product.register(categories, seller.sellerId)))
+			}
 		}
 	}
 	
 	def addProduct = Action.async { implicit request =>
-		withSessionId { implicit sellerId =>
-			withJsonBody[ProductDto] { implicit product =>
-				service.addProduct transform {
+		withSeller[SellerDto] { seller =>
+			withJsonBody[ProductDto] { product =>
+				service.addProduct(seller.sellerId, product) transform {
 					case Success(_) => Try(Ok(Json.toJson(true)))
 					case Failure(e) => Try(Ok(Json.toJson(Map("error" -> e.getMessage))))
 				}
@@ -118,12 +150,42 @@ class SellerController @Inject() (protected val dbConfigProvider: DatabaseConfig
 		}
 	}
 	
-	def getProduct = Action.async { implicit request =>
-		withSessionId { implicit sellerId =>
-			service.getProductList transform {
-				case Success(products) => Try(Ok(Json.toJson(products)))
+	def getProducts = Action.async { implicit request =>
+		withSeller[SellerDto] { seller =>
+			val st = System.nanoTime()
+			service.getProductList(seller.sellerId) transform {
+				case Success(products) => {
+					val ed = System.nanoTime()
+					val time = (ed-st)/1000000
+					println(s"상품 수: ${products.size} , 걸린 시간 ${time}ms")
+					Try(Ok(views.html.seller.product.product_list(products)))
+				}
 				case Failure(e) => Try(Ok(Json.toJson(Map("error" -> e.getMessage))))
 			}
 		}
 	}
+	
+	def getProductStock = Action.async { implicit request =>
+		withSeller[SellerDto] { seller =>
+			withJsonBody[StockDto] { stock =>
+				service.getProductStock(stock.productId) transform {
+					case Success(result) => Try(Ok(Json.toJson(result)))
+					case Failure(e) => Try(Ok(Json.toJson(Map("error" -> e.getMessage))))
+				}
+			}
+		}
+	}
+	
+	def updateStock = Action.async { implicit request =>
+		withSeller[SellerDto] { seller =>
+			withJsonBody[StockDto]	{ stock =>
+				service.updateStock(stock.productStockId, stock.stock) transform {
+					case Success(result) => Try(Ok(Json.toJson(result)))
+					case Failure(e) => Try(Ok(Json.toJson(Map("error" -> e.getMessage))))
+				}
+			}
+		}
+	}
+	
+	
 }
