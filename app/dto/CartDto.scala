13f3b72bc44b8ec3cf5e4e4ac8c4b281676c0950
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-04-03 16:55:37 +0900

    ADD: Json 데이터 읽을 때 필요한 묵시적 writes, reads 객체 선언, non_user_carts 테이블로부터 객체를 생성하는 newInstance2 메소드 선언

--- app/dto/CartDto.scala
+++ app/dto/CartDto.scala
@@ -1,8 +1,8 @@
 package dto
 
 import java.util.Date
-import play.api.libs.json.Json
 import models.Tables._
+import play.api.libs.json.Json
 
 case class CartDto(userId: String,
 				   var cartId: Int,
@@ -24,6 +24,16 @@ case class CartDto(userId: String,
 }
 
 object CartDto{
+	
+	implicit val itemWrites = Json.writes[ProductOptionItemDto]
+	implicit val itemReads = Json.reads[ProductOptionItemDto]
+
+	implicit val cartWrites = Json.writes[CartDto]
+	implicit val cartReads = Json.reads[CartDto]
+	
 	def newInstance(cart: Carts#TableElementType) =
-		new CartDto(cart.userId, cart.cartId, cart.name, cart.productId, cart.price, cart.quantity, cart.addedDate, Nil)
+		new CartDto(cart.userId, cart.cartId, cart.name, cart.productId, cart.price, cart.quantity, new Date(), Nil)
+		
+	def newInstance2(cart: NonUserCarts#TableElementType) =
+		new CartDto(cart.idToken, cart.nonUserCartId, cart.name, cart.productId, cart.price, cart.quantity, new Date(), Nil)
 }
