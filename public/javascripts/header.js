015afa468a2aa4b5531e2dd476bd0fb7c58f3f36
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-04-04 20:55:05 +0900

    ADD: 헤더 템플릿 스크립트

---
+++ public/javascripts/header.js
@@ -0,0 +1,24 @@
+const categoryBox = document.querySelector('div.category');
+const keywordBox = document.querySelector('input');
+
+window.onload = () => {
+    fetch("/category")
+        .then(response => response.json())
+        .then(json => {
+            json.forEach((cat, index) => {
+                let option = document.createElement('option');
+                option.setAttribute('value', cat[0])
+                option.label = cat[1]
+                categoryBox.querySelector('select.category').append(option)
+            })
+        })
+}
+
+keywordBox.addEventListener('submit', () => {
+    let category = document.querySelector("select.category")
+    let code = category.options[category.selectedIndex].value
+    let keyword = document.querySelector("input[name='keyword']").value
+
+    const url = new URL("http://localhost:9000/search/"+code+"/"+keyword)
+    location.href = url.href
+})
