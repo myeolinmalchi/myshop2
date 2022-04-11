b8eed30ea923352b7c581952245164db9c52676d
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-04-11 22:28:42 +0900

    ADD: 유효성 검사시 필요한 패턴 및 에러메세지 구문 여기로 옮김

--- app/dto/UserDto.scala
+++ app/dto/UserDto.scala
@@ -2,6 +2,7 @@ package dto
 
 import models.Tables._
 import play.api.libs.json.Json
+import scala.collection.mutable.Map
 
 case class UserDto(userId: String,
 				   userPw: String,
@@ -21,6 +22,33 @@ object UserDto{
 	implicit val userWrites = Json.writes[UserDto]
 	implicit val userReads = Json.reads[UserDto]
 	
+	val USER_ID = "userId"
+	val USER_PW = "userPw"
+	val NAME = "name"
+	val EMAIL = "email"
+	val PHONE = "phonenumber"
+	
+	val PATTERNS: Map[String, String] = Map (
+		USER_ID -> "^[a-z]+[a-z0-9]{5,19}$",
+		USER_PW -> "^(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$",
+		NAME -> "^[ㄱ-힣]+$",
+		EMAIL -> "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$",
+		PHONE -> "^\\d{3}-\\d{3,4}-\\d{4}$"
+	)
+	
+	val NONE_MATCH_EXCEPTION: Map[String, Exception] = Map (
+		USER_ID -> new IllegalArgumentException("유효하지 않은 아이디입니다."),
+		USER_PW -> new IllegalArgumentException("유효하지 않은 비밀번호입니다."),
+		NAME -> new IllegalArgumentException("유효하지 않은 이름입니다."),
+		EMAIL -> new IllegalArgumentException("유효하지 않은 이메일입니다."),
+		PHONE -> new IllegalArgumentException("유효하지 않은 전화번호입니다."),
+	)
+	
+	val OVERLAP_EXCEPTION: Map[String, Exception] = Map (
+		USER_ID -> new IllegalArgumentException("이미 존재하는 계정입니다."),
+		EMAIL -> new IllegalArgumentException("이미 존재하는 이메일입니다.")
+	)
+	
 	def newEntity(user: Users#TableElementType) =
 		new UserDto(user.userId, user.userPw, user.name, user.email, user.phonenumber)
 		
@@ -30,6 +58,8 @@ object UserDto{
 			  email: String,
 			  phonenumber: String) =
 		new UserDto(userId, userPw, name, email, phonenumber)
+		
+	def empty = new UserDto("", "", "", "", "")
 }
 
 object AddressDto{
