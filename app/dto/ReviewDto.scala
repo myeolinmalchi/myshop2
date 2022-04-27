dc8bc7ad18c898c46b64bac50974069a00e4b1df
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-04-27 23:42:24 +0900

    ADD: 내용 추가

--- app/dto/ReviewDto.scala
+++ app/dto/ReviewDto.scala
@@ -1,6 +1,27 @@
 package dto
 
-class ReviewDto {
+import java.util.Date
+import models.Tables.Reviews
+import play.api.libs.json.Json
 
+case class ReviewDto(productId: Int,
+					 reviewId: Int,
+					 userId: String,
+					 rating: Int,
+					 title: String,
+					 content: String,
+					 comment: String,
+					 reviewDate: Date,
+					 recommend: Int) {
 	
+}
+
+object ReviewDto {
+	implicit val reviewWrite = Json.writes[ReviewDto]
+	implicit val reviewRead = Json.reads[ReviewDto]
+	
+	implicit def newInstance(r: Reviews#TableElementType): ReviewDto = {
+		new ReviewDto(r.productId, r.reviewId, r.userId, r.rating, r.title,
+			r.content, r.comment, r.reviewDate, r.recommend)
+	}
 }
