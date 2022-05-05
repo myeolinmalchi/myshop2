8cf760ba2c0740fa30c69230b52a61c651c14c72
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-05-06 02:29:38 +0900

    ADD: 장바구니 추가시 유효성 검사 로직 추가

--- app/services/user/CartService.scala
+++ app/services/user/CartService.scala
@@ -1,19 +1,29 @@
 package services.user
 
+import cats.data.OptionT
 import com.google.inject.ImplementedBy
-import dto.{CartDto, UserDto}
+import common.validation.ValidationResultLib
+import dto.{CartDto, CartRequestDto, UserRequestDto}
+import play.api.data.validation.ValidationResult
+import play.api.mvc.Result
 import scala.concurrent.Future
+import services.user.CartService.CartInsertionFailure
 
 @ImplementedBy(classOf[CartServiceImpl])
-trait CartService{
+trait CartService extends ValidationResultLib[Future]{
 	
-	def addCart(implicit cart: CartDto): Future[Int]
+	def addCart(implicit cart: CartRequestDto): Future[Either[CartInsertionFailure, Int]]
 	def updateQuantity(cartId: Int, quantity: Int): Future[Int]
-	def addQuantity(cartId: Int): Future[Int]
-	def subQuantity(cartId: Int): Future[Int]
 	def getCarts(userId: String): Future[List[CartDto]]
-	def getCart(cartId: Int): Future[CartDto]
+	def getCart(cartId: Int): Future[Option[CartDto]]
 	def deleteCart(cartId: Int): Future[Int]
-	def checkOwnCart(userId: String, cartId: Int): Future[Boolean]
+	def checkOwnCart(userId: String, cartId: Int)
+					(result: => Future[Result]): Future[Result]
 	
+}
+
+object CartService {
+	sealed abstract class CartInsertionFailure
+	case class OutOfStock(stock: Int) extends CartInsertionFailure
+	case object IncorrectProductId extends CartInsertionFailure
 }
--- app/services/user/CartServiceImpl.scala
+++ app/services/user/CartServiceImpl.scala
@@ -1,27 +1,25 @@
 package services.user
 
-import dto.{CartDto, UserDto}
+import cats.data.OptionT
+import cats.implicits.toTraverseOps
+import common.validation.ValidationResultLib
+import dto.{CartDto, CartRequestDto}
 import javax.inject.{Inject, Singleton}
 import models.{CartModel, ProductModel}
+import play.api.mvc.Result
+import play.api.mvc.Results.{Forbidden, NotFound}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.language.postfixOps
+import services.user.CartService._
 
 @Singleton
 class CartServiceImpl @Inject() (cartModel: CartModel,
 								 productModel: ProductModel)
-								(implicit ec: ExecutionContext) extends CartService {
+								(implicit ec: ExecutionContext) extends CartService with ValidationResultLib[Future]{
 	
 	
 	private val outOfStockException = (stock: Int) =>
 		Future.failed(new Exception(s"재고가 부족합니다! (남은 수량: ${stock})"))
-	
-	override def addCart(implicit cart: CartDto): Future[Int] = {
-		val is = cart.itemList.map(_.productOptionItemId)
-		productModel checkStock(is, cart.quantity) flatMap {
-			case (stock, false) => outOfStockException(stock)
-			case (_, true) => cartModel addCart cart
-		}
-	}
 	
 	override def updateQuantity(cartId: Int, quantity: Int): Future[Int] = {
 		implicit val id: Int = cartId
@@ -33,23 +31,52 @@ class CartServiceImpl @Inject() (cartModel: CartModel,
 		}
 	}
 	
-	override def addQuantity(cartId: Int): Future[Int] =
-		cartModel addQuantity cartId
-	
-	override def subQuantity(cartId: Int): Future[Int] =
-		cartModel subQuantity cartId
-	
 	override def getCarts(userId: String): Future[List[CartDto]] =
 		cartModel getCartsByUserId userId
 	
-	override def getCart(cartId: Int): Future[CartDto] =
+	override def getCart(cartId: Int): Future[Option[CartDto]] =
 		cartModel getCartByCartId cartId
 	
 	override def deleteCart(cartId: Int): Future[Int] =
 		cartModel deleteCart cartId
 		
-	override def checkOwnCart(userId: String, cartId: Int): Future[Boolean] =
-		cartModel getCartByCartId cartId map { cart =>
-			cart.userId == userId
+	override def checkOwnCart(userId: String, cartId: Int)
+					 (result: => Future[Result]): Future[Result] =
+		getCart(cartId) flatMap {
+			case Some(cart) if cart.userId == userId => result
+			case Some(_) => Future(Forbidden)
+			case None => Future(NotFound)
 		}
+		
+	private def checkCorrectProductId(itemId: Int, productId: Int): Future[Boolean] =
+		productModel getProductIdByItemId itemId map {
+			case Some(pid) if pid == productId => true
+			case Some(_) => false
+			case None => false
+		}
+		
+	private def checkAllCorrectProductId(cart: CartRequestDto): ValidationResult[CartInsertionFailure, Unit] =
+		ValidationResult.ensureM(
+			cart.itemList traverse { itemId =>
+				checkCorrectProductId(itemId, cart.productId)
+			} map(_.forall(is => is)),
+			onFailure = IncorrectProductId
+		)
+		
+	private def checkStock(implicit cart: CartRequestDto): ValidationResult[CartInsertionFailure, Unit] =
+		ValidationResult(
+			productModel getStockByItemList cart.itemList flatMap { stock =>
+				ValidationResult.ensure(
+					stock >= cart.quantity,
+					onFailure = OutOfStock(stock)
+				).value
+			}
+		)
+		
+	override def addCart(implicit cart: CartRequestDto): Future[Either[CartInsertionFailure, Int]] =
+		(for {
+			_ <- checkAllCorrectProductId(cart)
+			_ <- checkStock
+		} yield ()) onSuccess(cartModel insertCart cart)
+	
 }
