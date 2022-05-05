24b360dc0186c9bf3f8d7308c949f52ad340927d
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-05-06 02:26:22 +0900

    MODIFY: 사용자 인증 로직 수정

--- app/services/user/AuthService.scala
+++ app/services/user/AuthService.scala
@@ -1,13 +1,27 @@
 package services.user
 
-import dto.UserDto
+import com.google.inject.ImplementedBy
+import dto.{UserDto, UserRequestDto}
 import play.api.mvc.{AnyContent, Request, Result}
 import scala.concurrent.{ExecutionContext, Future}
 
+@ImplementedBy(classOf[AuthServiceImpl])
 trait AuthService {
-	
 	def withUserAuth(id: String)(f: UserDto => Future[Result])
 					(implicit request: Request[AnyContent], ec: ExecutionContext,
 					 accountService: AccountService): Future[Result]
 	
+}
+
+object AuthService {
+	sealed abstract class AuthResult
+	case class AuthSuccess[T](value: T) extends AuthResult // 인증 성공
+	sealed abstract class AuthFailure extends AuthResult
+	
+	case object InvalidToken extends AuthFailure // 유효하지 않은 토큰
+	case object ExpiredToken extends AuthFailure // 유효기간이 지난 토큰
+	case object IncorrectAuth extends AuthFailure // 사용자 권한이 아님
+	case object IncorrectID extends AuthFailure // 두 ID가 일치하지 않음
+	case object NotExistID extends AuthFailure // db 상에 존재하지 않는 ID
+	case object NoToken extends AuthFailure // 토큰이 없음(사용자가 아님)
 }
--- app/services/user/AuthServiceImpl.scala
+++ app/services/user/AuthServiceImpl.scala
@@ -1,31 +1,22 @@
 package services.user
 
-import cats.data.OptionT
+import cats.data.{EitherT, OptionT}
 import common.validation.ValidationResultLib
-import dto.UserDto
+import dto.{UserDto, UserRequestDto}
 import javax.inject.Inject
 import models.UserModel
 import pdi.jwt.{JwtClaim, JwtJson}
 import play.api.libs.json.Json
-import play.api.mvc.Results.Unauthorized
+import play.api.mvc.Results.{Forbidden, NotFound, Unauthorized}
 import play.api.mvc.{AnyContent, Request, Result}
 import restcontrollers.Common.{ROLE_USER, algo, key}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
+import services.user.AuthService._
 
 class AuthServiceImpl @Inject() () (implicit ec: ExecutionContext, userModel: UserModel)
 		extends AuthService {
 	
-	sealed abstract class AuthResult[T](value: T)
-	case class AuthSuccess[T](value: T) extends AuthResult // 인증 성공
-	case class AuthFailure(msg: String) extends AuthResult
-	
-	case object InvalidToken extends AuthFailure("") // 유효하지 않은 토큰
-	case object ExpiredToken extends AuthFailure("") // 유효기간이 지난 토큰
-	case object IncorrectAuth extends AuthFailure("") // 사용자 권한이 아님
-	case object IncorrectID extends AuthFailure("") // 두 ID가 일치하지 않음
-	case object NotExistID extends AuthFailure("") // db 상에 존재하지 않는 ID
-	case object NoToken extends AuthFailure("") // 토큰이 없음(사용자가 아님)
 	case class UserAuth(id: String = "")
 					   (implicit request: Request[AnyContent], ec: ExecutionContext)
 			extends ValidationResultLib[Future] {
@@ -62,8 +53,8 @@ class AuthServiceImpl @Inject() () (implicit ec: ExecutionContext, userModel: Us
 			)
 		}
 		
-		def getUser: Future[Either[AuthFailure, AuthSuccess[UserDto]]] = {
-			val result = (for {
+		def validation: Future[Either[AuthFailure, AuthSuccess[UserDto]]] = {
+			val result: ValidationResult[AuthFailure, Unit] = (for {
 				token <- OptionT.fromOption[Try](request.headers.get("Authorization"))
 				claims <- OptionT.liftF(JwtJson.decode(token, key, Seq(algo)))
 				(role, id) = getJsonContent(claims)
@@ -73,8 +64,8 @@ class AuthServiceImpl @Inject() () (implicit ec: ExecutionContext, userModel: Us
 				_ <- checkRole(role)
 				_ <- checkUserId(id)
 				_ <- checkUserExist(id)
-			} yield ()).getOrElse(ValidationResult.failed(NoToken)) match {
-				case Success(result: ValidationResult[AuthFailure, Unit]) => result
+			} yield ()).getOrElse(ValidationResult.failed[AuthFailure, Unit](NoToken)) match {
+				case Success(rs: ValidationResult[AuthFailure, Unit]) => rs
 				case Failure(_) => ValidationResult.failed(InvalidToken)
 			}
 			
@@ -85,31 +76,22 @@ class AuthServiceImpl @Inject() () (implicit ec: ExecutionContext, userModel: Us
 			)
 		}
 		
-		def unAuthErrorJson(msg: String): Future[Result] =
-			Future(Unauthorized(Json.toJson(Map("error" -> msg))))
-		
-		def auth(authSuccess: UserDto => Future[Result],
-				 invalidToken: => Future[Result] = unAuthErrorJson("유효하지 않은 토큰입니다."),
-				 expiredToken: => Future[Result] = unAuthErrorJson("유효기간이 지난 토큰입니다."),
-				 incorrectAuth: => Future[Result] = unAuthErrorJson("사용자 권한이 아닙니다."),
-				 incorrectID: => Future[Result] = unAuthErrorJson("사용자 정보가 일치하지 않습니다."),
-				 notExistID: => Future[Result] = unAuthErrorJson("존재하지 않는 사용자입니다."),
-				 noToken: => Future[Result] = unAuthErrorJson("인증 정보를 찾을 수 없습니다.")): Future[Result] =
-			getUser flatMap {
+		def auth(authSuccess: UserDto => Future[Result]): Future[Result] =
+			validation flatMap {
 				case Right(result: AuthSuccess[UserDto]) => authSuccess(result.value)
-				case Left(result) => result match {
-					case InvalidToken => invalidToken
-					case ExpiredToken => expiredToken
-					case IncorrectAuth => incorrectAuth
-					case IncorrectID => incorrectID
-					case NotExistID => notExistID
-					case NoToken => noToken
-				}
+				case Left(InvalidToken) => Future(Unauthorized)
+				case Left(ExpiredToken) => Future(Unauthorized)
+				case Left(IncorrectAuth) => Future(Forbidden)
+				case Left(IncorrectID) => Future(Forbidden)
+				case Left(NotExistID) => Future(NotFound)
+				case Left(NoToken) => Future(Unauthorized)
 			}
+			
 	}
 	
 	override def withUserAuth(id: String)(f: UserDto => Future[Result])
 					(implicit request: Request[AnyContent], ec: ExecutionContext,
 					 accountService: AccountService): Future[Result] =
 		UserAuth(id).auth(f)
+		
 }
